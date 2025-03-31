dd5e6800221a44bd5242b829b415f31c75fb97c4
Robin Edquist <robin.edquist@helo.se>
2025-03-31 22:02:06 +0200

    fix image path

--- lib/photos.ts
+++ lib/photos.ts
@@ -20,7 +20,7 @@ export async function getPhotos(): Promise<Photo[]> {
     // Return mock data with varied sizes
 
     return [
-        {id: "1", src: "/DSCF5311.jpg", },
+        {id: "1", src: "DSCF5311.jpg", },
     ]
 }
 
